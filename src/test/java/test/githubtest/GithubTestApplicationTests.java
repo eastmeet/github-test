af4d30e1645067834eb551091135136221965a35
eastmeet <ys932184@gmail.com>
2024-01-26 13:23:06 +0900

    first commit

---
+++ src/test/java/test/githubtest/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
